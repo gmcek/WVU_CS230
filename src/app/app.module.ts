88f1ad7eccf173c2d7418db75654daefce851c6b
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-16 18:49:33 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './header/top-nav-bar.component';
+import { UserDropdownComponent } from './header/user-dropdown.component';
+import { PictureMenuComponent } from './header/picture-menu.component';
+import { WeeklyFeaturedGiftsComponent } from './header/weekly-featured-gifts.component';
+import { GiftsForCardsComponent } from './header/gifts-for-cards.component';
+import { LayoutMainComponent } from './layout/layout-main.component';
+import { GiftsCardComponent } from './body/playlist-card.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    UserDropdownComponent,
+    PictureMenuComponent,
+    WeeklyFeaturedGiftsComponent,
+    GiftsForCardsComponent,
+    LayoutMainComponent,
+    GiftsCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
