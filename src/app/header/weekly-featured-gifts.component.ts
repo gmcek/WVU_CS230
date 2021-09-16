851ff05ebf658cde0b899569db52aca3a22ff332
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-16 19:03:33 -0400

    Create weekly-featured-gifts.component.ts

---
+++ src/app/header/weekly-featured-gifts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-weekly-featured-gifts',
+    templateUrl: 'weekly-featured-gifts.component.html',
+    styleUrls: ['./weekly-featured-gifts.component.css']
+})
+export class WeeklyFeaturedGiftsComponent {
+
+}
