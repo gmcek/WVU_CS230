1403ca3c08b434c96a1f0c570d8b64531ae39c83
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-16 18:58:58 -0400

    Create picture-menu.component.ts

---
+++ src/app/header/picture-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-picture-menu',
+    templateUrl: 'picture-menu.component.html',
+    styleUrls: ['./picture-menu.component.css']
+})
+export class PictureMenuComponent {
+
+}
