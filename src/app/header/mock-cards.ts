5b146ed5eee87670f80bbe93fa2bf384682472d5
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-16 18:57:22 -0400

    Create mock-cards.ts

---
+++ src/app/header/mock-cards.ts
@@ -0,0 +1,23 @@
+
+export const mock_cards = [
+    {
+        title: "Gifts for Her",
+        imagePath: "./assets/GiftsForHer.PNG"
+    },
+    {
+        title: "Gifts for Him",
+        imagePath: "./assets/GiftsForHim.PNG"
+    },
+    {
+        title: "Gifts for Pets",
+        imagePath: "./assets/GiftsForPets.PNG"
+    },
+    {
+        title: "Gifts for Kids",
+        imagePath: "./assets/GiftsForKids.PNG"
+    },
+    {
+        title: "Birthday gifts",
+        imagePath: "./assets/BirthdayGifts.PNG"
+    }
+]
