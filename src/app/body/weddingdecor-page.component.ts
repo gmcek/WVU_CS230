3911860b1bc20483b93b553515f3157d8e90a196
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:48:26 -0400

    Create weddingdecor-page.component.ts

---
+++ src/app/body/weddingdecor-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-weddingdecor-page",
+    templateUrl: "weddingdecor-page.component.html"
+})
+export class WeddingDecorPageComponent {
+
+}
