38e439556be9b392210096a60eab90bab86b68d3
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:47:19 -0400

    Create picture-menu.component.ts

---
+++ src/app/body/picture-menu.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-picture-menu",
+    templateUrl: "picture-menu.component.html",
+    styleUrls: ["picture-menu.component.css"]
+})
+export class PictureMenuComponent {
+
+}
