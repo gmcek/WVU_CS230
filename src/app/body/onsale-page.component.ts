5e8ec7b13cee78857ffeb1c8249d0f01b4868c90
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:44:11 -0400

    Create onsale-page.component.ts

---
+++ src/app/body/onsale-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-onsale-page",
+    templateUrl: "onsale-page.component.html"
+})
+export class OnSalePageComponent {
+
+}
