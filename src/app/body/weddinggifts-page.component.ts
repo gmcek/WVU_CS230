5589e4d45a9fab154b63843570f4380876c022cd
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:49:57 -0400

    Create weddinggifts-page.component.ts

---
+++ src/app/body/weddinggifts-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-weddinggifts-page",
+    templateUrl: "weddinggifts-page.component.html"
+})
+export class WeddingGiftsPageComponent {
+
+}
