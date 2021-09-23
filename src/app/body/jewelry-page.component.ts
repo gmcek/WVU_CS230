44c085d6c3ac4a83d45c49c073eb1d5dd6efa72e
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:43:12 -0400

    Create jewelry-page.component.ts

---
+++ src/app/body/jewelry-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-jewelry-page",
+    templateUrl: "jewelry-page.component.html"
+})
+export class JewelryPageComponent {
+
+}
