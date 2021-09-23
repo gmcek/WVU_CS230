8e1b39e5851ff9ff8b57d4f27d009b004dc2346f
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:41:28 -0400

    Create bridesmaidgifts-page.component.ts

---
+++ src/app/body/bridesmaidgifts-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-bridesmaidgifts-page",
+    templateUrl: "bridesmaidgifts-page.component.html"
+})
+export class BridesmaidGiftsPageComponent {
+
+}
