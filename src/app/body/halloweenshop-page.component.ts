5578bd598012d8bbb43662330aea0b547af2dc3b
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:42:24 -0400

    Create halloweenshop-page.component.ts

---
+++ src/app/body/halloweenshop-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-halloweenshop-page",
+    templateUrl: "halloweenshop-page.component.html"
+})
+export class HalloweenShopPageComponent {
+
+}
