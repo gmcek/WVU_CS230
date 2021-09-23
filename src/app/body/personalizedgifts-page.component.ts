591e177bbeef6bf27a626d4b1abc49ee770b99c7
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-23 18:45:13 -0400

    Create personalizedgifts-page.component.ts

---
+++ src/app/body/personalizedgifts-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "etsy-personalizedgifts-page",
+    templateUrl: "personalizedgifts-page.component.html"
+})
+export class PersonalizedGiftsPageComponent {
+
+}
