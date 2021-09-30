56c9dcae896ff53078475f6c00e59f29ee5a8277
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-30 12:49:29 -0400

    Create productCard.component.ts

---
+++ src/app/body/productCard.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from "@angular/core";
+
+@Component({
+    selector: "etsy-product-card",
+    templateUrl: "productCard.component.html"
+})
+export class ProductCardComponent {
+    @Input() 
+    title! :string;
+    @Input()
+    description! :string;
+    @Input()
+    imagePath!: string;
+}
