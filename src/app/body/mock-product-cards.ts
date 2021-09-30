7c6054e3b5790655c7cb9de98a3c9843550f9f14
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-30 12:48:06 -0400

    Create mock-product-cards.ts

---
+++ src/app/body/mock-product-cards.ts
@@ -0,0 +1,27 @@
+export const mock_productCards = [
+    {
+        title: "Anniversary Gift for Him,Personalized Wallet,Mens Wallet,Engraved Wallet,Leather Wallet,Custom Wallet,Boyfriend Gift for Men,Gift for Dad",
+        description: "$28.99",
+        imagePath: "./assets/Wallet.PNG"
+    },
+    {
+        title: "Birth Flower Birthday Gift Box, Personalized birthday ideas, birthday present, gift for best friend - BdayFlower-dEGB",
+        description: "$21.75",
+        imagePath: "./assets/BirthFlowerBirthday.PNG"
+    },
+    {
+        title: "Anniversary Gift For Him, Custom Leather Wallet, Personalized Wallet, Engraved Wallet, Gift For Boyfriend, Mens Wallet, Dad Birthday Gift",
+        description: "$22.14",
+        imagePath: "./assets/CustomWallet.PNG"
+    },
+    {
+        title: "Personalized Charcuterie Planks and Beer Flights - 4 Styles and Gift Sets Available",
+        description: "$57.95",
+        imagePath: "./assets/PersonalizedCharcuteriePlanksAndBeerFlights.PNG"
+    },
+    {
+        title: "Custom Cutting Board - 10 Style - 9 Designs Newly Wed Gifts, Anniversary, Wedding Gift Custom Cutting Boards | Housewarming, Christmas Gift",
+        description: "$16.95",
+        imagePath: "./assets/CuttingBoard.PNG"
+    }
+]
