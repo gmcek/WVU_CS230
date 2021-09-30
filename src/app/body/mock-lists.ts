e9cb6dc42d8cad85589e1082190fd3ccd800f29b
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-30 12:47:34 -0400

    Create mock-lists.ts

---
+++ src/app/body/mock-lists.ts
@@ -0,0 +1,27 @@
+export const mock_lists = [
+    {
+        title: "Gifts for Her",
+        description: "",
+        imagePath: "./assets/GiftsForHer.PNG"
+    },
+    {
+        title: "Gifts for Him",
+        description: "",
+        imagePath: "./assets/GiftsForHim.PNG"
+    },
+    {
+        title: "Gifts for Pets",
+        description: "",
+        imagePath: "./assets/GiftsForPets.PNG"
+    },
+    {
+        title: "Gifts for Kids",
+        description: "",
+        imagePath: "./assets/GiftsForKids.PNG"
+    },
+    {
+        title: "Birthday gifts",
+        description: "",
+        imagePath: "./assets/BirthdayGifts.PNG"
+    }
+]
