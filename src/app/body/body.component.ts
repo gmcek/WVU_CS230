7a03f510ab9b44770001851a594e78164e2899d1
gmcek <89211335+gmcek@users.noreply.github.com>
2021-09-30 12:45:32 -0400

    Update body.component.ts

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,17 +1,24 @@
 import { Component } from "@angular/core";
-import { mock_playlists } from "./mock-playlists";
-import { Playlist } from "./playlist.model";
+import { mock_lists } from "./mock-lists";
+import { Card } from "./list.model";
+import { mock_productCards } from "./mock-product-cards";
+import { ProductCard } from "./productCard.model";
 
 @Component({
     selector: "etsy-body",
     templateUrl: "body.component.html"
 })
 export class BodyComponent {
-    playlists:Playlist[] = [];
+    lists:Card[] = [];
+    productcards:ProductCard[] =[];
 
     constructor() {
-        for (var playlist of mock_playlists) {
-            this.playlists.push(new Playlist(playlist));
+        for (var list of mock_lists) {
+            this.lists.push(new Card(list));
+        }
+
+        for (var productCard of mock_productCards) {
+            this.productcards.push(new ProductCard(productCard));
         }
     }
 }
