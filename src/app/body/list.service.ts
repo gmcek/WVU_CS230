084ced4dae6adbb5cb120288bb26eab70e851915
gmcek <89211335+gmcek@users.noreply.github.com>
2021-10-21 17:56:32 -0400

    Update list.service.ts

--- src/app/body/list.service.ts
+++ src/app/body/list.service.ts
@@ -1,23 +1,3 @@
-/*import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { list } from "./list.model";
-
-@Injectable({providedIn: 'root'})
-export class listService {
-    private baseUrl:string = 'https://etsy-app-4d3b6-default-rtdb.firebaseio.com/lists';
-    private listEndpoint:string = 'list.json';
-    
-    constructor(private http: HttpClient) {
-
-    }
-
-    getUserInfo() {
-        console.log(this.baseUrl + this.listEndpoint);
-        return this.http.get<list>(this.baseUrl + this.listEndpoint);
-    }
-}
-*/
-
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 
