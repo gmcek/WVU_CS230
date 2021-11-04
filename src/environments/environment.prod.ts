0c15807caa1376eac2a067005f5a12279a78dddf
gmcek <89211335+gmcek@users.noreply.github.com>
2021-11-04 15:11:20 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'etsy-app-4d3b6',
+    appId: '1:756333680160:web:baf01f596681b312948e45',
+    databaseURL: 'https://etsy-app-4d3b6-default-rtdb.firebaseio.com',
+    storageBucket: 'etsy-app-4d3b6.appspot.com',
+    apiKey: 'AIzaSyDnkL58makxRsHoH_t1_AP2dmfeGXkG95g',
+    authDomain: 'etsy-app-4d3b6.firebaseapp.com',
+    messagingSenderId: '756333680160',
+  },
+  production: true
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,25 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'etsy-app-4d3b6',
+    appId: '1:756333680160:web:baf01f596681b312948e45',
+    databaseURL: 'https://etsy-app-4d3b6-default-rtdb.firebaseio.com',
+    storageBucket: 'etsy-app-4d3b6.appspot.com',
+    apiKey: 'AIzaSyDnkL58makxRsHoH_t1_AP2dmfeGXkG95g',
+    authDomain: 'etsy-app-4d3b6.firebaseapp.com',
+    messagingSenderId: '756333680160',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
